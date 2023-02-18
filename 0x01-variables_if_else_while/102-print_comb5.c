d1feff172b62e782fc871218c9b2a43eccd14c46
dng-23 <dariknegussie4@gmail.com>
2023-02-18 05:33:02 +0000

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,29 +9,38 @@
 int main(void)
 {
 
-	int num = 48, num2, num3;
+	int num = 48, num2, num3 = 48, num4;
 
-	while (num <= 57)
+	while (num3 <= 57)
 	{
-		num2 = num + 1;
-		while (num2 <= 57)
+		num4 = num3;
+		while (num <= 57)
 		{
-			num3 = num2 + 1;
-			while (num3 <= 57)
+			num2 = num + 1;
+			while (num2 <= 57 && num4 <= 57)
 			{
-				putchar (num);
-				putchar (num2);
-				putchar (num3);
-				if (num != 55)
+				while (num2 <= 57)
 				{
-					putchar (',');
+					putchar (num3);
+					putchar (num);
 					putchar (' ');
+					putchar (num4);
+					putchar (num2);
+					if (num3 != 57 || num != 56)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+					num2++;
 				}
-				num3++;
+				num4++;
+				num2 = 48;
 			}
-			num2++;
+			num++;
+			num4 = num3;
 		}
-		num++;
+		num3++;
+		num = 48;
 	}
 
 	putchar ('\n');
