a3ebffaefb67659b73c54219d83139f6a9f61823
dng-23 <dariknegussie4@gmail.com>
2023-02-28 16:25:24 +0000

    fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,15 @@ void puts_half(char *str)
 {
 	int len = 0, half;
 
-	while (*str != '\0')
+	while (str[len] != '\0')
 	{
 		len++;
-		str++;
 	}
 	if (len % 2 == 0)
 		half = len / 2;
 	else
 		half = (len - 1) / 2;
+	half++;
 	while (str[half] != '\0')
 	{
 		_putchar(str[half]);
