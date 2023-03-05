a1d0240ba48c269adcfef09e9e122bcc54f5461f
dng-23 <dariknegussie4@gmail.com>
2023-03-05 05:25:54 +0000

    fix _atoi func

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,11 +10,15 @@ int _atoi(char *s)
 {
 	int i = 0, digit = 0, sign = 1;
 
+	while (s[i] == ' ')
+		i++;
 	if (s[i] == '-')
 	{
 		sign = -1;
 		++i;
 	}
+	else if (s[i] == '+')
+		 ++i;
 	for (; s[i] >= '0' && s[i] <= '9'; ++i)
 		digit = digit * 10 + (s[i] - '0');
 	return (digit * sign);
