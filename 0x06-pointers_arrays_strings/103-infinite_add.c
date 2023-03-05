9f2d9d220d0d4365af4fbc6e6b01e694e3b36bab
dng-23 <dariknegussie4@gmail.com>
2023-03-05 08:46:25 +0000

    fixed

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,60 +10,42 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char carry, sum, first, second;
-	int i = 0, j = 0;
+	char carry = 0, temp, sum;
+	int i = 0, j = 0, initial = 0;
 
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
 		j++;
-	if (i >= size_r || j >= size_r)
+	if (i > size_r || j > size_r)
 		return (0);
-	if (i > j)
-		size_r = size_r - (size_r - 1 - i);
-	else
-		size_r = size_r - (size_r - 1 - j);
 	i--;
 	j--;
-	while (size_r > 0)
+	while (initial < size_r - 1  && (i >= 0 || j >= 0 || carry > 0))
 	{
-		first = n1[i];
-		second = n2[j];
-		if (i < 0)
-			first = '0';
-		if (j < 0)
-			second = '0';
-		if (i < 0 && j < 0)
-			break;
-		if (carry == '1')
-		{
-			sum = (first + carry) - 48;
-			first = sum;
-			carry = '0';
-			if (sum > 57)
-			{
-				sum -= 10;
-				first = sum;
-				carry = '1';
-			}
-		}
-		sum = (first + second) - 48;
-		if (sum > 57)
-		{
-			sum -= 10;
-			carry = '1';
-		}
-		r[size_r - 1] = sum;
-		size_r--;
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - 48;
+		if (j >= 0)
+			sum += n2[j] - 48;
+		carry = sum / 10;
+		r[initial] = sum % 10 + 48;
+		initial++;
 		i--;
 		j--;
 	}
-	if (carry == '1')
+	if (carry > 0 && initial < size_r - 1)
+		r[initial++] = carry + '0';
+	r[initial] = '\0';
+	i = 0;
+	initial--;
+	while (i < initial)
 	{
-		if (size_r != 0)
-			r[size_r - 1] = carry;
-		else
-			return (0);
+		temp = r[i];
+		r[i] = r[initial];
+		r[initial] = temp;
+		i++;
+		initial--;
 	}
 	return (r);
 }
