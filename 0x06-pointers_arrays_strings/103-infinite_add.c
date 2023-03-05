5e4d90af2680aafcdc5fe723c125ab0a3291a56c
dng-23 <dariknegussie4@gmail.com>
2023-03-05 09:37:56 +0000

    fixed

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,18 +10,18 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char carry = 0, temp, sum;
-	int i = 0, j = 0, initial = 0;
+	int i = 0, j = 0, k = 0, sum, carry = 0;
+	char temp;
 
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
 		j++;
-	if (i > size_r || j > size_r)
+	if (i >= size_r || j >= size_r)
 		return (0);
 	i--;
 	j--;
-	while (initial < size_r - 1  && (i >= 0 || j >= 0 || carry > 0))
+	while (i >= 0 || j >= 0 || carry)
 	{
 		sum = carry;
 		if (i >= 0)
@@ -29,23 +29,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (j >= 0)
 			sum += n2[j] - 48;
 		carry = sum / 10;
-		r[initial] = sum % 10 + 48;
-		initial++;
+		r[k] = sum % 10 + '0';
 		i--;
 		j--;
+		k++;
 	}
-	if (carry > 0 && initial < size_r - 1)
-		r[initial++] = carry + '0';
-	r[initial] = '\0';
-	i = 0;
-	initial--;
-	while (i < initial)
+	r[k] = '\0';
+	if (k > size_r || (k == size_r && r[0] != '1'))
+		return (0);
+	for (i = 0, j = k - 1; i < j; i++, j--)
 	{
 		temp = r[i];
-		r[i] = r[initial];
-		r[initial] = temp;
-		i++;
-		initial--;
+		r[i] = r[j];
+		r[j] = temp;
 	}
 	return (r);
 }
