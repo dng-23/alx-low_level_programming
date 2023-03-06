13c5be5dd353296ecf11a38dd9d268054f91e93d
dng-23 <dariknegussie4@gmail.com>
2023-03-06 14:28:47 +0000

    fix betty

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * set_string: sets the value of a pointer to a char.
+ * set_string- sets the value of a pointer to a char.
  * @s: pointer to pointer of char
  * @to: char pointer
  * Return: nothing
