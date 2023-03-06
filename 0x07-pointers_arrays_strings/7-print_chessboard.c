1e1d6782eec8d51c170333ba0508a340299adcf3
dng-23 <dariknegussie4@gmail.com>
2023-03-06 14:25:05 +0000

    task_6 pushed

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard- prints the chessboard
+ * @a: array
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j = 0;
+
+	while (a[i][7])
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
