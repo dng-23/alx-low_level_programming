6b108b9db92ba7787a01ba948ebd647a5185755f
dng-23 <dariknegussie4@gmail.com>
2023-03-15 13:28:26 +0000

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(char *) * size);
+	if (!ptr)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,7 +17,7 @@ char **strtow(char *str)
 		return (NULL);
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' && (str[i + 1] != ' ') || (i = 0 && str[i] != ' '))
+		if ((str[i] == ' ' && (str[i + 1] != ' ')) || (i = 0 && str[i] != ' '))
 		{
 			count++;
 		}
