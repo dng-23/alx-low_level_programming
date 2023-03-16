d924eaa505ba319ae23991f1e34d1f52260908df
dng-23 <dariknegussie4@gmail.com>
2023-03-16 02:09:54 +0000

    final

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,58 +1,53 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * strtow - splits a string into words.
  * @str: string pointer
  * Return: pointer to an array of strings or NULL
  */
+
 char **strtow(char *str)
 {
-	int count = 0, k = 0, i, letter, j, l;
-	char **ptr;
+	int count = 0, i = 0, length, j;
+	char *p = str, *word, **ptr;
 
-	if (*str == '\0' || str == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	while (*p != '\0')
 	{
-		while (str[i] == ' ')
-			i++;
-		if (str[i] != '\0')
+		for (; *p == ' '; p++)
+		if (*p != '\0')
 		{
 			count++;
-			while (str[i] && (str[i] != ' '))
-				i++;
+			for (; *p != '\0' && *p != ' '; p++);
 		}
 	}
 	ptr = malloc((count + 1) * sizeof(char *));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	for (p = str; *p != '\0'; i++)
 	{
-		while (str[i] == ' ')
-			i++;
-		if (str[i] != '\0')
+		for (; *p == ' '; p++)
+		if (*p != '\0')
 		{
-			l = i;
-			while (str[i] && (str[i] != ' '))
-				i++;
-			letter = i - l;
-			ptr[k] = malloc(sizeof(char) * (letter + 1));
-			if (ptr[k] == NULL)
+			word = p;
+			for (; *p != '\0' && *p != ' '; p++)
+			length = p - word;
+			ptr[i] = malloc((length + 1) * sizeof(char));
+			if (ptr[i] == NULL)
 			{
 				for (j = 0; j < i; j++)
 					free(ptr[j]);
 				free(ptr);
 				return (NULL);
 			}
-			for (j = 0; j < letter; l++, j++)
-				ptr[k][j] = str[l];
-			ptr[k][letter] = '\0';
-			k++;
-			letter = 0;
+			for (j = 0; j < length; j++)
+				ptr[i][j] = word[j];
+			ptr[i][length] = '\0';
 		}
 	}
-	ptr[k] = NULL;
+	ptr[count] = NULL;
 	return (ptr);
 }
+
