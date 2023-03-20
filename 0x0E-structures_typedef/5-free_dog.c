d090edb1988cb8cad2ea6116fcc29cba5a123827
dng-23 <dariknegussie4@gmail.com>
2023-03-20 17:21:46 +0000

    free dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees memory allocated to dogs
+ * @d: variable to be freed
+ * Return: Nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d);
+}
