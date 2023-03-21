2a0ce5066ffbd631d66049d212fdf9fb894126fe
dng-23 <dariknegussie4@gmail.com>
2023-03-21 06:42:32 +0000

    all the bugs are fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 
+void _strcpy(char *src, char *dest);
+
 /**
  * new_dog - creates a new dog.
  * @name: value for the first member
@@ -10,11 +12,41 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0, j = 0;
+	
 	dog_t *d = malloc(sizeof(dog_t));
-
-	d->name = name;
+	if (!d)
+		return (NULL);
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
+	d->name = malloc(sizeof(char) * i);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * j);
+	if (d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	_strcpy(name, d->name);
+	_strcpy(name, d->owner);
 	d->age = age;
-	d->owner = owner;
 
 	return (d);
+}
+void _strcpy(char *src, char *dest)
+{
+	int i = 0;
+	
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 }
