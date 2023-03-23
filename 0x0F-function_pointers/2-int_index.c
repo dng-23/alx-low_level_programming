6b59175739ea7d81a7b4e9fe859cb7023b56a124
dng-23 <dariknegussie4@gmail.com>
2023-03-23 07:46:14 +0000

    added condition if array is NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, res;
 
-	if (size < 1 || cmp == NULL)
+	if (size < 1 || cmp == NULL || array = NULL)
 		return (-1);
 
 	while (i < size)
