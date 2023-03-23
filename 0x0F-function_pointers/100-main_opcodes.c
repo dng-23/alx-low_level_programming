683d30f60846e5984a7fc4e2147846abbb6769d5
dng-23 <dariknegussie4@gmail.com>
2023-03-23 09:45:55 +0000

    prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes of its own main function.
+ * @ac: number of arguments passed
+ * @av: array of arguments passed
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	int num, i;
+	char (*m);
+
+	if (ac != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num = atoi(av[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	m = (char *)main;
+	for (i = 0; i < num - 1; i++)
+		printf("%02hhx ",m[i]);
+	printf("%02hhx\n",m[i]);
+	return (0);
+}
