fa9f6f75c50c44d2acc70634f67889ff4f5bb82d
dng-23 <dariknegussie4@gmail.com>
2023-03-27 05:10:09 +0000

    remove unnecessary lines

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,16 +9,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct print_format - Struct print_format
- *
- * @spec: The specifier
- * @f: The function associated
- */
-typedef struct print_format
-{
-	char spec;
-	void (*f)(va_list);
-} pr_f;
-
 #endif
