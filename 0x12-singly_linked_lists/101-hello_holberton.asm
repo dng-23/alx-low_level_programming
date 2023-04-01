120c4ab0735ce846d7b693ffe174bf6532efd24e
dng-23 <dariknegussie4@gmail.com>
2023-04-01 03:59:06 +0000

    assembly code to print Hello, Holberton, followed by a new line.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+section .data
+    message db "Hello, Holberton",10
+
+global main
+section .text
+	main:
+    	mov eax, 4
+    	mov ebx, 1
+    	mov ecx, message
+    	mov edx, 18
+    	int 0x80
+    	mov eax, 1
+    	xor ebx, ebx
+    	int 0x80
