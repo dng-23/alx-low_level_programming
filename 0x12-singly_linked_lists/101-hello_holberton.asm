bb2966ec50a965e955ac67db508ae1a2258600bc
dng-23 <dariknegussie4@gmail.com>
2023-04-01 02:05:02 +0000

    edit to output the correct string

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,14 @@
 section .data
     message db "Hello, Holberton",10
+    msglen equ $-message
+
 global main
 section .text
 	main:
     	mov eax, 1
     	mov edi, 1
     	mov esi, message
-    	mov edx, 18
+    	mov edx, msglen
     	syscall
     	mov eax, 60
     	xor edi, edi
