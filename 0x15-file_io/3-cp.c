9961615f5849e92353eb68dff583bebdc3ceb57f
dng-23 <dariknegussie4@gmail.com>
2023-04-10 20:12:12 +0000

    copy content of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
+void handle_error(int value, int type, char *filename);
 /**
- * create_file - creates file
- * @filename: pointer to file to create
- * @text_content: pointer to text
- * Return: 1 on success, -1 on failure
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of strings of arguments
+ * Return: Always 0
  */
-int main (int argc, char *argv)
+int main(int argc, char **argv)
 {
-	int fd;
-	ssize_t write_size;
+	int fd_file1, fd_file2;
+	ssize_t read_size, write_size, close_val;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -19,19 +21,45 @@ int main (int argc, char *argv)
 	}
 
 	if (!argv[1])
+		handle_error(-1, 0, argv[1]);
+
+	fd_file1 = open(argv[1], O_RDWR);
+	handle_error(fd_file1, 0, argv[1]);
+
+	fd_file2 = creat(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	handle_error(fd_file2, 1, argv[2]);
+	while (read_size != 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		read_size = read(fd_file1, buffer, 1024);
+		handle_error(read_size, 0, argv[1]);
+
+		write_size = write(fd_file2, buffer, read_size);
+		handle_error(write_size, 1, argv[2]);
 	}
-	fd = creat(filename, S_IRUSR | S_IWUSR);
-	if (fd < 0)
-		return (-1);
-	if (!text_content)
-		write_size = write(fd, empty_file, 1);
-	else
-		write_size = write(fd, text_content, strlen(text_content));
-	close(fd);
-	if (write_size == -1)
-		return (-1);
-	return (1);
+	close_val = close(fd_file1);
+	handle_error(close_val, 2, argv[2]);
+	return (0);
+}
+
+/**
+ * handle_error - error handler for the main function
+ * @fd_value: value of file descrepter
+ * @type: type of error with int value
+ * @filename: name of the file
+ * Return: nothing
+ */
+void handle_error(int fd_value, int type, char *filename)
+{
+	char *msg[] = {"Error: Can't read from file", "Error: Can't write to file",
+	     "Error: Can't close fd"};
+	int ex[3] = {98, 99, 100};
+
+	if (fd_value == -1)
+	{
+		if (type != 2)
+			dprintf(STDERR_FILENO, "%s %s\n", msg[type], filename);
+		else
+			dprintf(STDERR_FILENO, "%s %d\n", msg[type], fd_value);
+		exit(ex[type]);
+	}
 }
