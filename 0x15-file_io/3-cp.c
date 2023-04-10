25ff9e58f7f569a371dcfdead903545afd8a91b9
dng-23 <dariknegussie4@gmail.com>
2023-04-10 20:26:20 +0000

    copy content of one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-void handle_error(int value, int type, char *filename);
+void handle_error(int value, int type, char *filename, int fd1, int fd2);
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -21,23 +21,23 @@ int main(int argc, char **argv)
 	}
 
 	if (!argv[1])
-		handle_error(-1, 0, argv[1]);
+		handle_error(-1, 0, argv[1], -1, -1);
 
 	fd_file1 = open(argv[1], O_RDWR);
-	handle_error(fd_file1, 0, argv[1]);
+	handle_error(fd_file1, 0, argv[1], fd_file1, -1);
 
 	fd_file2 = creat(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	handle_error(fd_file2, 1, argv[2]);
+	handle_error(fd_file2, 1, argv[2], fd_file1, fd_file2);
 	while (read_size != 0)
 	{
 		read_size = read(fd_file1, buffer, 1024);
-		handle_error(read_size, 0, argv[1]);
+		handle_error(read_size, 0, argv[1], fd_file1, fd_file2);
 
 		write_size = write(fd_file2, buffer, read_size);
-		handle_error(write_size, 1, argv[2]);
+		handle_error(write_size, 1, argv[2], fd_file1, fd_file2);
 	}
 	close_val = close(fd_file1);
-	handle_error(close_val, 2, argv[2]);
+	handle_error(close_val, 2, argv[2], fd_file1, fd_file2);
 	return (0);
 }
 
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
  * @filename: name of the file
  * Return: nothing
  */
-void handle_error(int fd_value, int type, char *filename)
+void handle_error(int fd_value, int type, char *filename, int fd1, int fd2)
 {
 	char *msg[] = {"Error: Can't read from file", "Error: Can't write to file",
 	     "Error: Can't close fd"};
@@ -60,6 +60,10 @@ void handle_error(int fd_value, int type, char *filename)
 			dprintf(STDERR_FILENO, "%s %s\n", msg[type], filename);
 		else
 			dprintf(STDERR_FILENO, "%s %d\n", msg[type], fd_value);
+		if (fd1 >= 0)
+			close(fd1);
+		if (fd2 >= 0)
+			close(fd2);
 		exit(ex[type]);
 	}
 }
