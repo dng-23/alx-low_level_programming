45ad22c37fd0aadebfafdc1d3905f203fd8709d7
dng-23 <dariknegussie4@gmail.com>
2023-04-13 01:15:02 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 	if (!text_content)
-		write_size = write(fd, empty_file, 1);
+		write_size = write(fd, empty_file, 0);
 	else
 		write_size = write(fd, text_content, strlen(text_content));
 	close(fd);
